e8ed2c4f83a08042235feb71a677858ada60fc0c
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2023-07-25 07:18:33 +0530

    check, uncheck, remove some comments, format

--- public/order_list_js/app.js
+++ public/order_list_js/app.js
@@ -1,105 +1,98 @@
 let tasks = [];
 
 function addTask() {
-  const taskInput = document.getElementById('taskInput');
-  const taskText = taskInput.value.trim();
+    const taskInput = document.getElementById('taskInput');
+    const taskText = taskInput.value.trim();
 
-  if (taskText === '') {
-    alert('Please enter a task.');
-    return;
-  }
+    if (taskText === '') {
+        alert('Please enter a task.');
+        return;
+    }
 
-  taskInput.value = '';
+    taskInput.value = '';
 
-  const task = {
-    id: Date.now(),
-    text: taskText,
-  };
+    const task = {
+        id: Date.now(),
+        text: taskText,
+    };
 
-  tasks.push(task);
-  updateTaskList();
+    tasks.push(task);
+    updateTaskList();
 }
 
-function updateTaskListOld() {
-  const taskList = document.getElementById('taskList');
-  taskList.innerHTML = '';
 
-  tasks.forEach(task => {
-    const listItem = document.createElement('li');
-    listItem.innerHTML = `
-      <span>${task.text}</span>
-      <button onclick="moveUp(${task.id})" ${tasks.indexOf(task) === 0 ? 'disabled' : ''}>Move Up</button>
-      <button onclick="moveDown(${task.id})" ${tasks.indexOf(task) === tasks.length - 1 ? 'disabled' : ''}>Move Down</button>
-      <button onclick="removeTask(${task.id})">Remove</button>
-    `;
-    taskList.appendChild(listItem);
-  });
-}
 
 function updateTaskList() {
     const taskList = document.getElementById('taskList');
     taskList.innerHTML = '';
-  
+
     tasks.forEach(task => {
-      const listItem = document.createElement('li');
-      listItem.innerHTML = `
+        const listItem = document.createElement('li');
+        listItem.innerHTML = `
+        <input type="checkbox" onchange="toggleTask(${task.id})" ${task.checked ? 'checked' : ''}>
         <button onclick="moveUp(${task.id})" ${tasks.indexOf(task) === 0 ? 'disabled' : ''}>
-        <i class="fas fa-arrow-up"></i> <!-- Font Awesome icon for move up -->
+          <i class="fas fa-arrow-up"></i>
         </button>
         <button onclick="moveDown(${task.id})" ${tasks.indexOf(task) === tasks.length - 1 ? 'disabled' : ''}>
-        <i class="fas fa-arrow-down"></i> <!-- Font Awesome icon for move down -->
+          <i class="fas fa-arrow-down"></i>
         </button>
         <button onclick="removeTask(${task.id})">
-        <i class="fas fa-trash-alt"></i> <!-- Font Awesome icon for remove -->
+          <i class="fas fa-trash-alt"></i>
         </button>
-        <span>${task.text}</span>
+        <span style="text-decoration: ${task.checked ? 'line-through' : 'none'}">${task.text}</span>
       `;
-      taskList.appendChild(listItem);
+        taskList.appendChild(listItem);
     });
-  }
+}
+
+function toggleTask(id) {
+    const task = tasks.find(task => task.id === id);
+    task.checked = !task.checked;
+    updateTaskList();
+}
 
 function moveUp(id) {
-  const taskIndex = tasks.findIndex(task => task.id === id);
+    const taskIndex = tasks.findIndex(task => task.id === id);
 
-  if (taskIndex > 0) {
-    const temp = tasks[taskIndex - 1];
-    tasks[taskIndex - 1] = tasks[taskIndex];
-    tasks[taskIndex] = temp;
-    updateTaskList();
-  }
+    if (taskIndex > 0) {
+        const temp = tasks[taskIndex - 1];
+        tasks[taskIndex - 1] = tasks[taskIndex];
+        tasks[taskIndex] = temp;
+        updateTaskList();
+    }
 }
 
 function moveDown(id) {
-  const taskIndex = tasks.findIndex(task => task.id === id);
+    const taskIndex = tasks.findIndex(task => task.id === id);
 
-  if (taskIndex < tasks.length - 1) {
-    const temp = tasks[taskIndex + 1];
-    tasks[taskIndex + 1] = tasks[taskIndex];
-    tasks[taskIndex] = temp;
-    updateTaskList();
-  }
+    if (taskIndex < tasks.length - 1) {
+        const temp = tasks[taskIndex + 1];
+        tasks[taskIndex + 1] = tasks[taskIndex];
+        tasks[taskIndex] = temp;
+        updateTaskList();
+    }
 }
 
 function removeTask(id) {
-  tasks = tasks.filter(task => task.id !== id);
-  updateTaskList();
+    tasks = tasks.filter(task => task.id !== id);
+    updateTaskList();
 }
 
 function saveOrder() {
-  // You can add code here to save the order of tasks to a database or localStorage.
-  // For this example, we'll just log the tasks to the console.
-  localStorage.setItem('tasks', JSON.stringify(tasks));
-  console.log(tasks);
+    // You can add code here to save the order of tasks to a database or localStorage.
+    // For this example, we'll just log the tasks to the console.
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    console.log(tasks);
 }
 
 
 function loadTasksFromLocalStorage() {
     const storedTasks = localStorage.getItem('tasks');
     if (storedTasks) {
-      tasks = JSON.parse(storedTasks);
-      updateTaskList();
+        tasks = JSON.parse(storedTasks);
+        updateTaskList();
     }
-  }
-  
-  // Load tasks from local storage when the page loads
-  window.addEventListener('load', loadTasksFromLocalStorage);
+}
+
+// Load tasks from local storage when the page loads
+window.addEventListener('load', loadTasksFromLocalStorage);
